9806659e294fe465a548aacfb06641f4b7144342
Adnan Qureshi <adnanquresh262@gmail.com>
2025-11-30 17:45:17 +0530

    Footer added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from "./component/sections/Navbar";
+import Navigation from "./component/ui/Navigation";
 import Hero from "./component/ui/Hero";
 import About from "./component/sections/About";
 export default function HomePage() {
   return (
     <main>
   
-      <Navbar />
+      <Navigation />
       <Hero />
       <About />
     </main>
